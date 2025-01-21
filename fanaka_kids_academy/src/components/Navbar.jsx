e58139d06902f03e2fa864bd7f3e231598e55621
tobias <tobias.omondi@student.moringaschool.com>
2025-01-21 16:00:20 +0300

    started project

---
+++ fanaka_kids_academy/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { AiFillTikTok, AiOutlineWhatsApp } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+    {/* main container */}
+    <div className='bg-white w-screen'>
+            <ul className='bg-red-800 w-48 text-4xl flex justify-evenly p-2 ml-auto'>
+                <li>
+                    <a href='#' className='text-black hover:text-blue-700'><AiFillTikTok /></a>
+                </li>
+                <li>
+                    <a href='#'  className='text-green-600 hover:text-green-300'><AiOutlineWhatsApp /></a>
+                </li>
+            </ul>
+    </div>
+    {/* main navbar container */}
+
+
+
+    <div className='bg-orange-600 p-10 flex items-center justify-between'>
+        {/* logo for the website */}
+        <p>FANAKA KIDS</p>  
+        {/* links for the web pages */}
+        <div className=' text-white flex-1 text-center space-x-9'>
+            <Link to="/" >Home</Link>
+            <Link to="/about" >About Us</Link>
+            <Link to="/blog" >Blog</Link>
+            <Link to="/media" >Media</Link>
+            <Link to="/games" >Game</Link>
+        </div>
+        <div className=' text-white'>
+            <Link to="/contact" >Contact us</Link>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
