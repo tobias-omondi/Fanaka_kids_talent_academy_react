eb1b6aeb8659d34bb1ce0e28bc6ede066a25b1af
tobias <tobias.omondi@student.moringaschool.com>
2025-01-22 11:57:32 +0300

    navbar commponents

--- fanaka_kids_academy/src/components/Navbar.jsx
+++ fanaka_kids_academy/src/components/Navbar.jsx
@@ -1,43 +1,85 @@
-import React from 'react'
-import { AiFillTikTok, AiOutlineWhatsApp } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
-import logo from '../assets/fanakakidslogo.jpeg'
+import React, { useState } from 'react';
+import { AiFillTikTok, AiOutlineWhatsApp } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import logo from '../assets/fanakakidslogo.jpeg';
+import { ChevronDown } from 'lucide-react';
 
 const Navbar = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropDownOpen(!isDropDownOpen);
+  };
+
   return (
     <>
-    {/* main container */}
-    <div className='bg-white shadow-2xl'>
-            <ul className='bg-red-700 w-48 text-3xl flex justify-evenly p-2 ml-auto shadow-2xl'>
-                <li>
-                    <a href='#' className='text-black hover:text-blue-700'><AiFillTikTok /></a>
-                </li>
-                <li>
-                    <a href='#'  className='text-green-600 hover:text-green-300'><AiOutlineWhatsApp /></a>
-                </li>
-            </ul>
-    </div>
-    {/* main navbar container */}
+      {/* Social icons section */}
+      <div className="bg-white shadow-1xl">
+        <ul className="bg-slate-200 w-60 text-3xl flex justify-evenly p-2 ml-auto shadow-2xl rounded-s-2xl">
+          <li>
+            <a href="#" className="text-black hover:text-gray-800">
+              <AiFillTikTok />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-green-600 hover:text-green-300">
+              <AiOutlineWhatsApp />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Main Navbar container */}
+      <div className="bg-orange-600 p-5 flex items-center justify-between shadow-2xl">
+        {/* Logo for the website */}
+        <img src={logo} alt="logo" className="w-14 rounded-e-md m-3 shadow-xl" />
+
+        {/* Links for the web pages */}
+        <div className="flex flex-1 justify-end items-center space-x-10 font-medium">
+          <Link to="/" className="text-white hover:text-red-700">
+            Home
+          </Link>
+          <Link to="/about" className="text-white hover:text-red-700">
+            About Us
+          </Link>
+          <Link to="/blog" className="text-white hover:text-red-700">
+            Blog
+          </Link>
+
+          {/* Dropdown for Media */}
+          <div className="relative">
+            <button
+              type="button"
+              onClick={toggleDropdown}
+              className="flex items-center text-white hover:text-red-700"
+            >
+              Media <ChevronDown className="ml-2" />
+            </button>
+
+            {/* Dropdown Menu */}
+            {isDropDownOpen && (
+              <div className="absolute left-0 mt-2 bg-orange-600 text-white shadow-lg rounded-md">
+                <Link to="/images" className="block px-4 py-2 hover:bg-orange-500">
+                  Images
+                </Link>
+                <Link to="/videos" className="block px-4 py-2 hover:bg-orange-500">
+                  Videos
+                </Link>
+              </div>
+            )}
+          </div>
 
 
-
-    <div className='bg-orange-600 p-3 flex items-center justify-between shadow-2xl'>
-        {/* logo for the website */}
-        <img src={logo} alt='logo' className='w-20 rounded-xl' /> 
-        {/* links for the web pages */}
-        <div className=' text-white flex-1 text-center space-x-9'>
-            <Link to="/" >Home</Link>
-            <Link to="/about" >About Us</Link>
-            <Link to="/blog" >Blog</Link>
-            <Link to="/media" >Media</Link>
-            <Link to="/games" >Game</Link>
+          <Link to="/games" className="text-white hover:text-red-700">
+            Game
+          </Link>
+          <Link to="/contact" className="text-white hover:text-red-700">
+            Contact Us
+          </Link>
         </div>
-        <div className=' text-white'>
-            <Link to="/contact" >Contact us</Link>
-        </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
