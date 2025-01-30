67852fe60634a9aacd36ec567dcddbb62a8b5b2f
tobias <tobias.omondi@student.moringaschool.com>
2025-01-30 14:13:20 +0300

    community file added

---
+++ fanaka_kids_academy/src/components/Community.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Community = ({ title, description, image }) => {
+  return (
+    <div className="mt-44 flex flex-col lg:flex-row items-center lg:items-start justify-between p-6 lg:p-12 rounded-lg shadow-lg">
+      {/* Text Content */}
+      <div className="w-full lg:w-1/2 text-center lg:text-left mb-8 lg:mb-0">
+        <h2 className="text-red-900 text-3xl lg:text-4xl font-bold mb-4">
+          {title}
+          <span className="block w-28 h-1 bg-yellow-500 mt-2"></span>
+        </h2>
+        <div className='underline-offset-auto'></div>
+        <p className="text-gray-700 text-lg lg:text-xl">
+          {description}
+        </p>
+      </div>
+
+      {/* Image */}
+      <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+        <img src={image} alt="A child dancing" className="w-72 h-96 lg:w-96 lg:h-96 object-cover rounded-lg shadow-md"/>
+      </div>
+    </div>
+  );
+};
+
+export default Community;
+// A community of young talents, <br/> offering Ballet, Chess and Public <br/> Speaking Perfomance
+// A vibrant community of young talent is comming together to show case their extraodinary abilities.
+//             From graceful movements of ballet dancers to the strategic minds of chess players and the captivating oratora, these children are poised to inspire and amaze. Their perfomance promise to be a delightful blend of 
+//             artistry, intellect, and eloquence
--- fanaka_kids_academy/src/components/Homepage.jsx
+++ fanaka_kids_academy/src/components/Homepage.jsx
@@ -2,12 +2,17 @@ import React, { Fragment } from 'react';
 import picone from '../assets/fanakapicone.jpeg';
 import pictwo from '../assets/fanaka2.jpeg'
 import picthree from '../assets/fanaka3.jpeg'
+import childimage from '../assets/community.jpeg'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Pagination , Autoplay, Navigation} from 'swiper/modules';
 import './slider.css'
+import { GiBallerinaShoes } from "react-icons/gi"
+import { FaChess } from "react-icons/fa6";
+import { PiMicrophoneStageDuotone } from "react-icons/pi";
+import Community from './Community';
 
 
 const Homepage = () => {
@@ -30,14 +35,14 @@ const Homepage = () => {
       >
         <SwiperSlide className='relative brightness-75'>
          <img src={picone}  alt="fanaka_kids_picture"  className="object-cover w-full h-full" />
-           <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 text-white'>
+           <div className='absolute top-1/2 md:top-96 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 text-white'>
             <h3 className='text-3xl md:text-5xl font-bold mb-4'>Welcome , Little Explorers!</h3>
              <button type='button' className='bg-black bg-opacity-60 hover:bg-opacity-90 text-white px-6 py-2 rounded-lg text-xl' > Learn More</button> </div>
         </SwiperSlide>
 
         <SwiperSlide className='relative brightness-75'>
          <img src={pictwo}  alt="fanaka_kids_picture"  className="object-cover w-full h-full" />
-           <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 text-white'>
+           <div className='absolute top-1/2 md:top-96  left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 text-white'>
             <h3 className='text-3xl md:text-5xl font-bold mb-4'>Adventure Awaits You!</h3>
              <button type='button' className='bg-black bg-opacity-60 hover:bg-opacity-90 text-white px-6 py-2 rounded-lg text-xl' > Learn More</button> </div>
         </SwiperSlide>
@@ -50,8 +55,25 @@ const Homepage = () => {
         </SwiperSlide>
       </Swiper>
       </div>
-
-
+        {/* Centered Container */}
+        <div className="absolute top-100 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 bg-orange-600 shadow-2xl rounded-lg py-4 md:py-0">
+        <div className=" max-h-48 w-full flex items-center justify-center md:px-28 ">
+          <h3 className="text-center text-white text-1xl md:text-4xl w-full font-light font-serif">
+            Best place <br /> for your child
+          </h3>
+        </div>
+        <div className='text-yellow-50 flex justify-center w-full gap-10 md:gap-52'>
+            <GiBallerinaShoes size={70} />
+            <FaChess size={70} />
+            <PiMicrophoneStageDuotone size={70} />
+          </div>
+      </div>
+      <Community 
+      title="A community of young talents,offering Ballet, Chess and Public Speaking Perfomance" 
+      description="A vibrant community of young talent is coming together to showcase their extraordinary abilities.
+From the graceful movements of ballet dancers to the strategic minds of chess players and the captivating orators, these children are poised to inspire and amaze. Their performances promise to be a delightful blend of artistry, intellect, eloquence, and public speaking."
+      image = {childimage}
+      />
     </div>
   );
 };
