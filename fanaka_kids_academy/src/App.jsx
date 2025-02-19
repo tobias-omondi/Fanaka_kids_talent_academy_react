62cf0d147007c17aa94d62dee3e914636cf97f42
tobias <tobias.omondi@student.moringaschool.com>
2025-02-19 15:07:17 +0300

    deleted dashboard files

--- fanaka_kids_academy/src/App.jsx
+++ fanaka_kids_academy/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
@@ -8,20 +8,13 @@ import MainBlog from './components/BlogPage/MainBlog';
 import ScrollToTop from './components/ScrollTop';
 import GalleryImagepage from './components/Media/GalleryImagepage';
 import GalleryVideos from './components/Media/GalleryVideos';
-import MainRoutes from './Dashboard/Main/MainRoutes';
+
 
 function AppContent() {
-  const location = useLocation();  
-  const hideNavbar = location.pathname.startsWith("/fanaka-dashboard"); // Hide navbar on dashboard pages
-
-  // Check if user is an admin
-  const isAdmin = localStorage.getItem("isAdmin") === "true"; 
-  console.log("Admin status:", isAdmin);  // Debug log to check admin status
-
   return (
     <>
       <ScrollToTop />
-      {!hideNavbar && <Navbar />}  {/* Hide Navbar on dashboard */}
+       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Aboutpage />} />
@@ -29,12 +22,8 @@ function AppContent() {
         <Route path="/blog" element={<MainBlog />} />
         <Route path="/images" element={<GalleryImagepage />} />
         <Route path="/videos" element={<GalleryVideos />} />
-        
-        <Route
-          path="/fanaka-dashboard/*"
-          element={<MainRoutes isAdmin={isAdmin} />}
-        />
       </Routes>
+      
     </>
   );
 }
@@ -44,6 +33,7 @@ function App() {
     <Router>
       <AppContent />
     </Router>
+    
   );
 }
 
